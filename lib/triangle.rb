08a7c283f69395d0d82fb361e1521ddce655cc1b
SANJAY K SAI <shachsan@hotmail.com>
2018-11-03 01:46:34 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,11 +8,11 @@ class Triangle
   end
 
   def equilateral
-    return true if side1 == side2 && side2 == side3
+    side1 == side2 && side2 == side3
   end
 
   def isosceles
-    return 
+    return true
   end
 
   def scalene
