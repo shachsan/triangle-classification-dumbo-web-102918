277d3bf68e4a0b00e2da1e29dad7be240762b2ec
SANJAY K SAI <shachsan@hotmail.com>
2018-11-03 01:57:58 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
   end
 
   def isosceles
-    ((side1 || side2)==side3) || ((side3 || side2)==side1) ? true 
+    ((side1 || side2)==side3) || ((side3 || side2)==side1) ? true : false
   end
 
   def scalene
